1b583ec67a99444f6783133c186b2719d75ede9e
Dominique Clark <melaydominique@gmail.com>
2023-03-28 17:06:10 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
+const App = () =>  {
+ const [zip, setZip] = useState('')
+ const [state, setState] = useState('')
+ const [city, setCity] = useState('')
+
+  const getWeather = () => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${state}&appid=8e6c1243aeb41a312c229cfda8bc51e3`).then((response) => {
+      console.log(response.data.main)
+      setCity(response.data.main)
+    })
+  }
+  const handleZip = (event) => {
+    setZip(event.target.value)
+  }
+  const handleState = (event) => {
+    setState(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getWeather()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather</h1>
+      <h2>Temperature: {city.temp}</h2>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='zip' onChange={handleZip}></input>
+        <input type='text' name='state' onChange={handleState}></input>
+        <input type='submit'></input>
+      </form>
     </div>
   );
 }
