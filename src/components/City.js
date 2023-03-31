e3e8cb11a14194f7ebd60e3c3afce4524eecdac3
Dominique Clark <melaydominique@gmail.com>
2023-03-31 16:02:09 -0400

    commit

--- src/components/City.js
+++ src/components/City.js
@@ -1,6 +1,19 @@
+import axios from 'axios'
+import { useState } from 'react'
 const City = (props) => {
+
+    const handleDelete = () => {
+        axios.delete(`http://localhost:3000/cities/${props.city._id}`).then(() => {
+            props.getCities();
+        })
+    }
     return(
-        <><h1>{props.city.name}</h1></>
+        <div className='citydisplay'>
+            {props.city ? <>
+            <h1>{props.city.name}</h1>
+            <button onClick={handleDelete}>Delete</button></>
+            : <></>}
+        </div>
     )
 }
 export default City
