46365e34d03ed0833a6033c8cb7a4d5cc430e234
Dominique Clark <melaydominique@gmail.com>
2023-03-31 15:35:33 -0400

    commit

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,23 +2,10 @@ import {useState} from 'react'
 import axios from 'axios'
 const moment = require('moment')
 
-const Add = () => {
+const Add = (props) => {
   const [zip, setZip] = useState('')
   const [state, setState] = useState('')
-  const [city, setCity] = useState({
-    name: '',
-    temp: 0,
-    tempMax: 0,
-    tempMin: 0,
-    tempFeels: 0,
-    time: '',
-    humidity: 0,
-    windSpeed: 0,
-    weatherMain: '',
-    weatherDescription: '',
-    weatherIcon: '',
-    cloud: 0,
-  })
+  const [city, setCity] = useState({})
 
   const handleZip = (event) => {
     setZip(event.target.value)
@@ -26,14 +13,14 @@ const Add = () => {
   const handleState = (event) => {
     setState(event.target.value)
   }
- 
+
   const handleSubmit = (event) => {
     event.preventDefault()
     axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${state}&appid=8e6c1243aeb41a312c229cfda8bc51e3&units=imperial`).then((response) => {
         const time = response.data.timezone
         const timeMin = time / 60
         const currentTime = moment().utcOffset(timeMin).format("h: mm A")
-        setCity({
+        const newCity = {
             name: response.data.name,
             temp: response.data.main.temp,
             tempMax: response.data.main.temp_max,
@@ -45,10 +32,16 @@ const Add = () => {
             weatherMain: response.data.weather[0].main,
             weatherDescription: response.data.weather[0].description,
             weatherIcon: response.data.weather[0].icon,
-            cloud: response.data.clouds.all,
+            cloud: response.data.clouds.all
+        }
+        setCity(newCity)
+        
+    }).then(() => {
+        axios.post('http://localhost:3000/cities', city).then(()=> {
+            props.getCities()
         })
     })
-    axios.post('http://localhost:3000/cities', city)
+ 
   }
 
     return (
@@ -58,8 +51,6 @@ const Add = () => {
                 <input type='text' name='state' onChange={handleState}></input>
                 <input type='submit'></input>
             </form>
-            <br/>
-            <p>{city.name}</p>
         </>
     )
 
